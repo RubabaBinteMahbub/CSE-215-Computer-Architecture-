382e2a8e2ff2633664021f809363572e333f5670
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-30 22:47:40 +0600

    Update 5.asm

--- mid assignment/C to MIPS/5.asm
+++ mid assignment/C to MIPS/5.asm
@@ -14,22 +14,22 @@ else
 
     # --- Outer if: if (A > B) ---
 slt $t0, $s1, $s0       # if B < A (meaning A > B), $t0 = 1; else $t0 = 0
-beq $t0, $zero, ELSE_A  # if $t0 == 0 (A <= B), jump to outer else (ELSE_A)
+beq $t0, $zero, ELSE_1  # if $t0 == 0 (A <= B), jump to outer else (ELSE_A)
 
     # --- Inner if: if (C > D) ---
     slt $t1, $s3, $s2   # if D < C (meaning C > D), $t1 = 1; else $t1 = 0
-    beq $t1, $zero, ELSE_C # if $t1 == 0 (C <= D), jump to inner else (ELSE_C)
+    beq $t1, $zero, ELSE_2 # if $t1 == 0 (C <= D), jump to inner else (ELSE_C)
     
     # Inner IF Body: Max = C
     move $s4, $s2       # Max = C
     j EXIT              # Jump to end of structures
 
-    ELSE_C:
+    ELSE_2:
     # Inner ELSE Body: Max = D
     move $s4, $s3       # Max = D
     j EXIT              # Jump to end of structures
 
-ELSE_A:
+ELSE_1:
 # Outer ELSE Body: Max = B
 move $s4, $s1           # Max = B
 
