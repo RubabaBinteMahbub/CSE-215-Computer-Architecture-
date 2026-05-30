b2b7e4fe22806c95ef8c0a695cad0ed7c84ee893
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-30 17:32:49 +0600

    Create 5.asm

---
+++ mid assignment/C to MIPS/5.asm
@@ -0,0 +1,37 @@
+if (A > B)
+{
+    if (C > D)
+        Max = C;
+    else
+        Max = D;
+}
+else
+{
+    Max = B;
+}
+
+# mapping: A=$s0, B=$s1, C=$s2, D=$s3, Max=$s4
+
+    # --- Outer if: if (A > B) ---
+slt $t0, $s1, $s0       # if B < A (meaning A > B), $t0 = 1; else $t0 = 0
+beq $t0, $zero, ELSE_A  # if $t0 == 0 (A <= B), jump to outer else (ELSE_A)
+
+    # --- Inner if: if (C > D) ---
+    slt $t1, $s3, $s2   # if D < C (meaning C > D), $t1 = 1; else $t1 = 0
+    beq $t1, $zero, ELSE_C # if $t1 == 0 (C <= D), jump to inner else (ELSE_C)
+    
+    # Inner IF Body: Max = C
+    move $s4, $s2       # Max = C
+    j EXIT              # Jump to end of structures
+
+    ELSE_C:
+    # Inner ELSE Body: Max = D
+    move $s4, $s3       # Max = D
+    j EXIT              # Jump to end of structures
+
+ELSE_A:
+# Outer ELSE Body: Max = B
+move $s4, $s1           # Max = B
+
+EXIT:
+# End of conditional block
